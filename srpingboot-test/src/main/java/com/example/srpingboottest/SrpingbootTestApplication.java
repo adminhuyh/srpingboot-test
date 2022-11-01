8ac8a398201b5bb23596f24dc4f1708fbb2818a5
huyuang <huyuhang@aixuexi.com>
2022-11-01 11:50:52 +0800

    commit

---
+++ srpingboot-test/src/main/java/com/example/srpingboottest/SrpingbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.srpingboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingbootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingbootTestApplication.class, args);
+    }
+
+}
---
+++ srpingboot-test/src/test/java/com/example/srpingboottest/SrpingbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srpingboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingbootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
